8eeae3c1265d0047f3c1541fbced366c08e3ef78
maaaaaaaaad <integral5137@gmail.com>
2023-05-14 20:36:01 +0900

    e

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 from dotenv import load_dotenv
 import os
 
@@ -6,6 +7,10 @@ load_dotenv()
 
 library_auth_api_key = os.environ.get('LIBRARY_AUTH_KEY')
 base_url = f'http://data4library.kr/api/loanItemSrch?authKey={library_auth_api_key}'
+url = f'{base_url}&startDt=2023-04-01&endDt=2023-04-21'
+
+response = requests.get(url)
+print(response.content)
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
 print(df.head())
