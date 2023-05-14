07f01485e17f4ec5f481d15eb98c7dee4dc67542
maaaaaaaaad <integral5137@gmail.com>
2023-05-14 20:28:44 +0900

    e

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import json
-
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+library_auth_api_key = os.environ.get('LIBRARY_AUTH_KEY')
+base_url = f'http://data4library.kr/api/loanItemSrch?authKey={library_auth_api_key}'
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
 print(df.head())
-print(df)
-
-d = {"name": "mad"}
-d_json = json.dumps(d, ensure_ascii=False)
-print(d_json)
-print(type(d_json))
+print(library_auth_api_key)
