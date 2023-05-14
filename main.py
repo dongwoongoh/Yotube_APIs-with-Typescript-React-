2f19572cf4c3f4efd15bed936618debb865e889f
maaaaaaaaad <integral5137@gmail.com>
2023-05-14 15:06:39 +0900

    e

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import json
+
 import pandas as pd
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
 print(df.head())
 print(df)
+
+d = {"name": "mad"}
+d_json = json.dumps(d, ensure_ascii=False)
+print(d_json)
+print(type(d_json))
