d7f7664615c7e7414fe1d18558b4b7fe608314b5
maaaaaaaaad <integral5137@gmail.com>
2023-05-16 21:13:04 +0900

    e

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
 df.to_json('2023_05_popular_books', force_ascii=False)
+
+print(df.head())
+
+loc = df.loc[[0, 1], ['도서명', '등록일자']]
+print(loc)
