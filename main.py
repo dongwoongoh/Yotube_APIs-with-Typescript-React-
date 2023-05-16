d6463f03caa7855a269c6c55f614d68e62f822c5
maaaaaaaaad <integral5137@gmail.com>
2023-05-16 21:04:43 +0900

    e

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-import json
-
 import pandas as pd
-import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-library_auth_api_key = os.environ.get('LIBRARY_AUTH_KEY')
-base_url = f'http://data4library.kr/api/loanItemSrch?authKey={library_auth_api_key}'
-url = f'{base_url}&startDt=2023-04-01&endDt=2023-04-21'
-
-response = requests.get(url)
-if response.status_code == 200:
-    content = response.content
-    text = response.text
-    print(text)
-    print(content)
-    try:
-        r_json = response.json()
-    except json.JSONDecodeError:
-        print('json decode error, standing wait api key')
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
-print(df.head())
-print(library_auth_api_key)
+df.to_json('2023_05_popular_books', force_ascii=False)
