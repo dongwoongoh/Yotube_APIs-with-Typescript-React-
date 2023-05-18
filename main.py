03759c60b56e18b9ba63e912eb266ba09664b15c
maaaaaaaaad <integral5137@gmail.com>
2023-05-18 20:40:22 +0900

    e

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
-df.to_json('2023_05_popular_books', force_ascii=False)
-
-selected_columns = df.columns != 'Unnamed: 13'
-ns_books = df.loc[:, selected_columns]
-print(ns_books.head())
+df.drop('Unnamed: 13', axis=1, inplace=True)
+print(df.head())
