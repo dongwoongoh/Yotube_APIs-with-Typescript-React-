33e5d46aaf9937c5c0da09256fc1a3f20b917a66
maaaaaaaaad <integral5137@gmail.com>
2023-05-18 20:42:56 +0900

    e

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='EUC_KR', low_memory=False)
 df.drop('Unnamed: 13', axis=1, inplace=True)
+df.dropna(axis=1, how='all', inplace=True)
 print(df.head())
