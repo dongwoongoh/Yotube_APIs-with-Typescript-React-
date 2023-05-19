9ecbb023d1e77be8678c27baa42b269951d6b9d6
maaaaaaaaad <integral5137@gmail.com>
2023-05-19 17:37:08 +0900

    e

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ df = pd.read_csv('./datas/namsan_library_popular_books_2023_05.csv', encoding='E
 df.drop('Unnamed: 13', axis=1, inplace=True)
 df.dropna(axis=1, how='all', inplace=True)
 print(df.head())
+
+selected_rows = df['출판사'] == '우리문학사'
+result = df[selected_rows]
+print(result)
